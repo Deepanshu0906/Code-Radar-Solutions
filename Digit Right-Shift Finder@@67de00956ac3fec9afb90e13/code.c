29bfa2ef6e49481b0ff47fd76899699106910bcc
Deepanshu Goyal <apsingla950@gmail.com>
2025-03-27 20:31:13 +0530

    Solution to Digit Right-Shift Finder

---
+++ Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+int digitalRightShift(int A){
+    int array[10000],num,i,count=0;
+    while(A>0){
+        array[i]=A%10;
+        i++;
+        num=A/10;
+    }
+    for(int j=i;j>0;j--){
+        count+=1;
+    }
+    for(int j=0;j<count;j++){
+        return array[j]
+    }
+}
